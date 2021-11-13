1c093ea83c2c5cab6f0a8b78bd4d711ca78d03b4
Roman <72631119+Useit14@users.noreply.github.com>
2021-11-13 12:32:07 +0300

    add done

--- js/js.js
+++ js/js.js
@@ -6,5 +6,40 @@ function swap(img){
 
 }
 
+function backSwap(){
+    let position
+    let src=document.images[0].src
+    let positions=src.split(".jpg")
+    let count=0
 
+    positions=positions[0].split("")
+    
+    for(let element in positions){
+        count++
+    }
+    position=positions[count-1]
+    position--
+    if(position<0){
+        position=3
+    }
+    document.images[0].src="./images/img"+position+".jpg";
+}
+function nextSwap(){
+    let position
+    let src=document.images[0].src
+    let positions=src.split(".jpg")
+    let count=0
+    
+    positions=positions[0].split("")
+    
+    for(let element in positions){
+        count++
+    }
+    position=positions[count-1]
+    position++
+    if(position>3){
+        position=0
+    }
+   document.images[0].src="./images/img"+position+".jpg";
+}
 
